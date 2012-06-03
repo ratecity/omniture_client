9fac71abaa02c8b7a9b4825ed1801e3ae52e512a
Robin Sharp <rob@sharp.id.au>
2012-06-04 08:49:26 +1000

    Catch the case when the client is not configured properly

--- lib/omniture_client.rb
+++ lib/omniture_client.rb
@@ -12,6 +12,7 @@ module OmnitureClient
     attr_accessor :aliases, :base_url, :ssl_url, :suite, :version
 
     def config(config_hash)
+      raise ArgumentError, "You must supply a configuration hash to OmnitureClient#config" if config_hash.nil
       config_hash.each do |key, val|
         send("#{key}=", val)
       end
